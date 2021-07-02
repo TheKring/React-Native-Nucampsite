c3e9a042df372870c83fbbcb1b755edc4fed32ce
Justin Kring <Justin.l.kring@gmail.com>
2021-07-02 12:24:51 -0500

    Gestures Part 1

--- components/CampsiteInfoComponent.js
+++ components/CampsiteInfoComponent.js
@@ -7,11 +7,14 @@ import {
 	Modal,
 	Button,
 	StyleSheet,
-} from "react-native";
+    Alert, 
+	PanResponder
+		} from "react-native";
 import { Card, Icon, Rating, Input } from "react-native-elements";
 import { connect } from "react-redux";
 import { baseUrl } from "../shared/baseUrl";
 import { postComment, postFavorite } from "../redux/ActionCreators";
+import * as Animatable from 'react-native-animatable';
 
 const mapStateToProps = (state) => {
 	return {
@@ -28,38 +31,76 @@ const mapDispatchToProps = {
 };
 
 function RenderCampsite(props) {
-	const { campsite } = props;
 
-	if (campsite) {
-		return (
-			<Card
-				featuredTitle={campsite.name}
-				image={{ uri: baseUrl + campsite.image }}
+    const {campsite} = props;
+
+	const recognizeDrag = ({dx}) => (dx < -200) ? true : false;
+
+	const panResponder = PanResponder.create({
+        onStartShouldSetPanResponder: () => true,
+        onPanResponderEnd: (e, gestureState) => {
+            console.log('pan responder end', gestureState);
+            if (recognizeDrag(gestureState)) {
+                Alert.alert(
+                    'Add Favorite',
+                    'Are you sure you wish to add ' + campsite.name + ' to favorites?',
+                    [
+                        {
+                            text: 'Cancel',
+                            style: 'cancel',
+                            onPress: () => console.log('Cancel Pressed')
+                        },
+                        {
+                            text: 'OK',
+                            onPress: () => props.favorite ?
+                                console.log('Already set as a favorite') : props.markFavorite()
+                        }
+                    ],
+                    { cancelable: false }
+                );
+            }
+            return true;
+        }
+    });
+
+    if (campsite) {
+        return (
+            <Animatable.View
+                animation='fadeInDown'
+                duration={2000}
+                delay={1000}
+                {...panResponder.panHandlers}>
+                <Card
+					featuredTitle={campsite.name}
+					image={{ uri: baseUrl + campsite.image }}
 			>
-				<Text style={{ margin: 10 }}>{campsite.description}</Text>
-				<View style={styles.cardRow}>
-					<Icon
-						name={props.favorite ? "heart" : "heart-o"}
-						type="font-awesome"
-						color="#f50"
-						raised
-						reverse
-						onPress={() =>
-							props.favorite
-								? console.log("Already set as a favorite")
-								: props.markFavorite()
-						}
-					/>
-					<Icon
-						name="pencil"
-						type="font-awesome"
-						color="#5637DD"
-						raised
-						reverse
-						onPress={() => props.onShowModal()}
-					/>
-				</View>
-			</Card>
+					<Text style={{ margin: 10 }}>
+						{campsite.description}
+					</Text>
+					<View style={styles.cardRow}>
+						<Icon
+							name={props.favorite ? "heart" : "heart-o"}
+							type="font-awesome"
+							color="#f50"
+							raised
+							reverse
+							onPress={() =>
+								props.favorite
+									? console.log("Already set as a favorite")
+									: props.markFavorite()
+							}
+						/>
+						<Icon
+							name="pencil"
+							type="font-awesome"
+							color="#5637DD"
+							raised
+							reverse
+							onPress={() => props.onShowModal()}
+						/>
+					</View>
+				</Card>
+            </Animatable.View>
 		);
 	}
 	return <View />;
@@ -69,7 +110,9 @@ function RenderComments({ comments }) {
 	const renderCommentItem = ({ item }) => {
 		return (
 			<View style={{ margin: 10 }}>
-				<Text style={{ fontSize: 14 }}>{item.text}</Text>
+				<Text style={{ fontSize: 14 }}>
+					{item.text}
+				</Text>
 				<Rating
 					imageSize={10}
 					readonly
@@ -79,21 +122,23 @@ function RenderComments({ comments }) {
 						paddingVertical: "5%",
 					}}
 				/>
-				<Text
-					style={{ fontSize: 12 }}
-				>{`-- ${item.author}, ${item.date}`}</Text>
+				<Text style={{ fontSize: 12 }}>
+					{`-- ${item.author}, ${item.date}`}
+				</Text>
 			</View>
 		);
 	};
 
 	return (
-		<Card title="Comments">
+        <Animatable.View animation='fadeInUp' duration={2000} delay={1000}>
+            <Card title='Comments'>
 			<FlatList
 				data={comments}
 				renderItem={renderCommentItem}
 				keyExtractor={(item) => item.id.toString()}
 			/>
-		</Card>
+		 </Card>
+        </Animatable.View>
 	);
 }
 
